74a21e092455bad2c3b0ba70cdeea8ad69756959
Chris Aquino <chris.aquino@gmail.com>
2017-08-01 18:23:38 -0400

    fixes the thing

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var request = require('request');
 
 var app = express();
 app.use('/', function(req, res) {
-  var url = apiServerHost + req.url;
+  var url = req.url.slice(1);
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  console.log(url)
   req.pipe(request(url)).pipe(res);
 });
 
