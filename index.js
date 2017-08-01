e5a6a887762580690a9b871f40aad7f6242e8329
Chris Aquino <chris.aquino@gmail.com>
2017-08-01 18:17:14 -0400

    does the thing

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var request = require('request');
 
 var app = express();
 app.use('/', function(req, res) {
-    var url = apiServerHost + req.url;
-    req.pipe(request(url)).pipe(res);
+  var url = apiServerHost + req.url;
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  req.pipe(request(url)).pipe(res);
 });
 
 app.listen(process.env.PORT || 3000);
